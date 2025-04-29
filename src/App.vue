1e27e0b125bc9db69ea12729ad5799b222c82cb5
josemarneto <jc.mpneto@gmail.com>
2025-04-29 16:47:49 -0300

    FEAT#2- Avanço das aplicações

--- src/App.vue
+++ src/App.vue
@@ -19,42 +19,42 @@ const item = ref([
         id: 3,
         titulo: 'City of Fallen Angels',
         autor: 'Cassandra Clare',
-        preco: 13,
+        preco: 13.94,
         capa: "public/imagens/livro3.png",
     },
     {
         id: 4,
         titulo: 'Nona the Ninth',
         autor: 'Cassandra Clare',
-        preco: ``,
+        preco: 16.84,
         capa: "public/imagens/livro4.png",
     },
     {
         id: 5,
         titulo: 'Harlem Shuffle',
         autor: 'Colson Whitehead',
-        preco: ``,
+        preco: 26.92,
         capa: "public/imagens/livro5.png",
     },
     {
         id: 6,
         titulo: 'Two Old Women',
         autor: 'Velma Wallis', 
-        preco: ``,
+        preco: 13.95,
         capa: "public/imagens/livro6.png",
     },
     {
         id: 7,
         titulo: 'Carrie Soto Is Back',
         autor: 'Taylor Jenkins Reid',
-        preco: ``,
+        preco: 26.04,
         capa: "public/imagens/livro7.png",
     },
     {
         id: 8,
         titulo: 'Book Lovers',
         autor: 'Emily Henry',
-        preco: ``,
+        preco: 15.81,
         capa: "public/imagens/livro8.png",
     },
 ])
@@ -100,12 +100,12 @@ const item = ref([
 
   <div class="pagina1">
         <div class="mudar">
-          <p>Autor de Abril</p>
+          <p class="autor">Autor de Abril</p>
           <h1>Eric-Emanuel Schmitt </h1>
           <p>Eric-Emmanuel Schmitt has been awarded more than 20 <br> literary prizes and distinctions, and in 2001 he received the <br> title of Chevalier des Arts et des Lettres. His books have been <br> translated into over 40 languages.</p>
-          <button>Acessar página do livro</button>
+          <a href="#">Acessar página do livro</a>
         </div>  
-        <div>
+        <div class="schmitt">
           <img src="/public/imagens/book.png" alt="book.png" style="display: block;">
         </div>
   </div> 
@@ -127,37 +127,39 @@ const item = ref([
         Mais vendidos
       </div>
     </div>
+    <hr>
   </section>
   <section class="lancamentos">
+    <h2 class="rapido">Lançamentos</h2>
     <div class="cima">
       <div>
         <img src="/public/imagens/Livro1.png" alt="Livro1.png">
         <h2>Chain of Iron: Volume 2</h2>
         <p>Cassandra Clare</p>
-        <p>R$23.24</p> <i class="fa-regular fa-heart"></i>
-        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+        <p>R$23.24</p> 
+        <a href="#"><i class="fa-solid fa-cart-shopping"></i>comprar</a>
       </div>
       <div>
         <img src="/public/imagens/Livro2.png" alt="Livro2.png">
         <h2>Chain of Thorns</h2>
         <p>Cassandra Clare</p>
-        <p>R$23.24</p> <i class="fa-regular fa-heart"></i>
-        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+        <p>R$23.24</p>
+        <a href="#"><i class="fa-solid fa-cart-shopping"></i>comprar</a>
       </div>
       
       <div>
         <img src="/public/imagens/Livro3.png" alt="Livro3.png">
         <h2>City of Fallen Angels</h2>
         <p>Cassandra Clare</p>
-        <p>R$13.94</p> <i class="fa-regular fa-heart"></i>
-        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+        <p>R$13.94</p> 
+        <a href="#"><i class="fa-solid fa-cart-shopping"></i>comprar</a>
       </div>
       <div>
         <img src="/public/imagens/Livro4.png" alt="Livro4.png">
         <h2>Nona the Ninth</h2>
         <p>Cassandra Clare</p>
-        <p>R$16.84</p> <i class="fa-regular fa-heart"></i>
-        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+        <p>R$16.84</p> 
+        <a href="#"><i class="fa-solid fa-cart-shopping"></i>comprar</a>
       </div>
     </div>
     <div class="baixo">
@@ -165,29 +167,29 @@ const item = ref([
         <img src="/public/imagens/Livro5.png" alt="Livro5.png">
         <h2>Harlem Shuffle</h2>
         <p>Colson Whitehead</p>
-        <p>R$26.92</p> <i class="fa-regular fa-heart"></i>
-        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+        <p>R$26.92</p> 
+        <a href="#"><i class="fa-solid fa-cart-shopping"></i>comprar</a>
       </div>
       <div>
         <img src="/public/imagens/Livro6.png" alt="Livro6.png">
         <h2>Two Old Women</h2>
         <p>Two Old Women</p>
-        <p>R$13.95</p> <i class="fa-regular fa-heart"></i>
-        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+        <p>R$13.95</p> 
+        <a href="#"><i class="fa-solid fa-cart-shopping"></i>comprar</a>
       </div>
       <div>
         <img src="/public/imagens/Livro7.png" alt="Livro7.png">
         <h2>Carrie Soto Is Back</h2>
         <p>Taylor Jenkins Reid</p>
-        <p>R$26.04</p> <i class="fa-regular fa-heart"></i>
-        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+        <p>R$26.04</p> 
+        <a href="#"><i class="fa-solid fa-cart-shopping"></i>comprar</a>
       </div>
       <div>
         <img src="/public/imagens/Livro8.png" alt="Livro8.png">
         <h2>Book Lovers</h2>
         <p>Emily Henry</p>
-        <p>R$15.81</p> <i class="fa-regular fa-heart"></i>
-        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+        <p>R$15.81</p> 
+        <a href="#"><i class="fa-solid fa-cart-shopping"></i>comprar</a>
       </div>
     </div>
   </section>
@@ -215,8 +217,57 @@ const item = ref([
 </template>
 
 <style scoped>
+  div.topo{
+    display: flex;
+    margin: 0 10vw;
+}
+  div.if{
+    margin: 1vw 1vw;
+  }
+  div p.apreco{
+    margin: 1vw 1vw;
+  }
+  div.pesquisa{
+    margin: 1vw 4vw; 
+  }
+  div.pesquisa input {
+    width: 20vw;
+  }
+  div.itens {
+    margin: 1vw 2.5vw;
+  }
+  div.icons {
+    margin: 1vw 2vw;
+    padding: 0.7vw;
+  }
+  div.pagina1 {
+    display: flex; 
+  }
+  div.pagina1 div.mudar {
+    margin: 4vw 10vw;
+  }
   div.pagina1 div.mudar h1{
     font-size: 2.5rem;
+  }
+  div.pagina1 div.mudar p.autor{
+    font-size: 1.3rem;
+    color: #27AE60;
+    border: #27AE60 solid;
+    border-radius: 4px;
+    border-width: 2px;
+    padding: 3px;
+  }
+  div.pagina1 div.mudar a {
+    text-decoration: none;
+    background-color: #27AE60;
+    font-size: 1.2rem;
+    padding: 16px;
+    border: #27AE60;
+    color: white;
+    margin: 3vw 0vw;
+  }
+  div.pagina1 div.schmitt {
+    margin: 4vw 10vw;
   }
   svg{
     width: 20px;
@@ -249,6 +300,12 @@ const item = ref([
     display: flex;
     margin: 5vw;
     justify-content: space-between;
+    text-decoration: none;
+  }
+  section.lancamentos div.cima a {
+    text-decoration: none;
+    border: #27AE60;
+    background-color: #27AE60;
   }
   section.lacamentos div.cima div h2{
     font-size: 1.4rem;
@@ -256,17 +313,26 @@ const item = ref([
   section.mais div.itenss {
     display: flex;
     justify-content: space-between;
-    width: 70%;
+    width: 97%;
   }
   section.mais hr{
     border-bottom: solid 20px;
   }
-  section.mais{
-    margin: 30px;
+  section.mais div.frete {
+    text-align: center;
   }
-  section
-  header div.icons{
+  section.mais{
+    margin: 50px;
+  }
+  section.mais hr{
+    border-bottom: solid 2px;
+  }
+  section header div.icons{
     color: #27AE60;
+  }
+  .rapido {
+    margin: 6vw 8vw 4vw 8vw;
+    font-size: 3rem;
   }
   header div.itens nav a{
     color: #7B7881;
