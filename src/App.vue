d8a6be767fc1773feb915786dcf66d5586d744c4
AfonsoJaning <afonsocjaning@gmail.com>
2025-04-29 13:45:22 -0300

    App Vue #2

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,285 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+const item = ref([
+{
+        id: 1,
+        titulo: 'Chain of Iron: Volume 2',
+        autor: 'Cassandra Clare',
+        preco: 23.24,
+        capa: "public/imagens/livro1.png",
+    },
+    {
+        id: 2,
+        titulo: 'Chain of Thorns',
+        autor: 'Cassandra Clare',
+        preco: 23.24,
+        capa: "public/imagens/livro2.png",
+    },
+    {
+        id: 3,
+        titulo: 'City of Fallen Angels',
+        autor: 'Cassandra Clare',
+        preco: 13,
+        capa: "public/imagens/livro3.png",
+    },
+    {
+        id: 4,
+        titulo: 'Nona the Ninth',
+        autor: 'Cassandra Clare',
+        preco: ``,
+        capa: "public/imagens/livro4.png",
+    },
+    {
+        id: 5,
+        titulo: 'Harlem Shuffle',
+        autor: 'Colson Whitehead',
+        preco: ``,
+        capa: "public/imagens/livro5.png",
+    },
+    {
+        id: 6,
+        titulo: 'Two Old Women',
+        autor: 'Velma Wallis', 
+        preco: ``,
+        capa: "public/imagens/livro6.png",
+    },
+    {
+        id: 7,
+        titulo: 'Carrie Soto Is Back',
+        autor: 'Taylor Jenkins Reid',
+        preco: ``,
+        capa: "public/imagens/livro7.png",
+    },
+    {
+        id: 8,
+        titulo: 'Book Lovers',
+        autor: 'Emily Henry',
+        preco: ``,
+        capa: "public/imagens/livro8.png",
+    },
+])
 </script>
 
 <template>
+<main>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="topo">
+      <div class="if">
+        <p>IFbooks</p>
+        </div>
+        <span class="barra"></span>
+        <div>
+        <p class="apreco">Apreço a <br>leitura</p>
+      </div>
+      <div class="pesquisa">
+        <input type="text" placeholder="Pesquisar">
+        <i class="fa-solid fa-magnifying-glass"></i>
+      </div>
+    <div class="itens">
+        <nav>
+            <a href="#">Termos</a>
+        
+            <a href="#">Devoluções</a>
+        
+            <a href="#">Envio</a>
+          
+            <a href="#">Equipe</a>
+        </nav>
     </div>
+    <div class="icons">
+      <i class="fa-solid fa-cart-shopping"></i>
+      <span></span>
+      <i class="fa-solid fa-heart"></i>
+      <span></span>
+      <i class="fa-solid fa-user"></i>
+    </div>
+    </div>
+    <hr>
   </header>
+  <section class="inico">
 
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="pagina1">
+        <div class="mudar">
+          <p>Autor de Abril</p>
+          <h1>Eric-Emanuel Schmitt </h1>
+          <p>Eric-Emmanuel Schmitt has been awarded more than 20 <br> literary prizes and distinctions, and in 2001 he received the <br> title of Chevalier des Arts et des Lettres. His books have been <br> translated into over 40 languages.</p>
+          <button>Acessar página do livro</button>
+        </div>  
+        <div>
+          <img src="/public/imagens/book.png" alt="book.png" style="display: block;">
+        </div>
+  </div> 
+  </section>
+  <section class="mais">
+    <div class="itenss">
+      <div class="frete">
+        <i class="fa-solid fa-truck"></i>
+        Frete grátis para SC
+      </div>
+      <hr>
+      <div>
+        <i class="fa-solid fa-star"></i>
+        Livros recomendados
+      </div>
+      <hr>
+      <div>
+        <i class="fa-solid fa-book-open"></i>
+        Mais vendidos
+      </div>
+    </div>
+  </section>
+  <section class="lancamentos">
+    <div class="cima">
+      <div>
+        <img src="/public/imagens/Livro1.png" alt="Livro1.png">
+        <h2>Chain of Iron: Volume 2</h2>
+        <p>Cassandra Clare</p>
+        <p>R$23.24</p> <i class="fa-regular fa-heart"></i>
+        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+      </div>
+      <div>
+        <img src="/public/imagens/Livro2.png" alt="Livro2.png">
+        <h2>Chain of Thorns</h2>
+        <p>Cassandra Clare</p>
+        <p>R$23.24</p> <i class="fa-regular fa-heart"></i>
+        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+      </div>
+      
+      <div>
+        <img src="/public/imagens/Livro3.png" alt="Livro3.png">
+        <h2>City of Fallen Angels</h2>
+        <p>Cassandra Clare</p>
+        <p>R$13.94</p> <i class="fa-regular fa-heart"></i>
+        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+      </div>
+      <div>
+        <img src="/public/imagens/Livro4.png" alt="Livro4.png">
+        <h2>Nona the Ninth</h2>
+        <p>Cassandra Clare</p>
+        <p>R$16.84</p> <i class="fa-regular fa-heart"></i>
+        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+      </div>
+    </div>
+    <div class="baixo">
+      <div>
+        <img src="/public/imagens/Livro5.png" alt="Livro5.png">
+        <h2>Harlem Shuffle</h2>
+        <p>Colson Whitehead</p>
+        <p>R$26.92</p> <i class="fa-regular fa-heart"></i>
+        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+      </div>
+      <div>
+        <img src="/public/imagens/Livro6.png" alt="Livro6.png">
+        <h2>Two Old Women</h2>
+        <p>Two Old Women</p>
+        <p>R$13.95</p> <i class="fa-regular fa-heart"></i>
+        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+      </div>
+      <div>
+        <img src="/public/imagens/Livro7.png" alt="Livro7.png">
+        <h2>Carrie Soto Is Back</h2>
+        <p>Taylor Jenkins Reid</p>
+        <p>R$26.04</p> <i class="fa-regular fa-heart"></i>
+        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+      </div>
+      <div>
+        <img src="/public/imagens/Livro8.png" alt="Livro8.png">
+        <h2>Book Lovers</h2>
+        <p>Emily Henry</p>
+        <p>R$15.81</p> <i class="fa-regular fa-heart"></i>
+        <button><i class="fa-solid fa-cart-shopping"></i>comprar</button>
+      </div>
+    </div>
+  </section>
+</main> 
+<footer>
+  <div class="ifbooks">
+    <h2>IFbooks</h2>
+    <i class="fa-brands fa-square-facebook"></i>
+    <i class="fa-brands fa-square-instagram"></i>
+    <i class="fa-brands fa-square-twitter"></i>
+  </div>
+  <div>
+    <h2>Contato</h2>
+    <i class="fa-solid fa-phone"></i> <p>+55 47 40045263</p>
+    <i class="fa-solid fa-clock"></i> <p>8h às 23h - Seg a Sex</p>
+    <i class="fa-solid fa-envelope"></i> <p>contato@ifbooks.com</p>
+    <img src="/public/imagens/paypal.png" alt="paypal.png">
+    <img src="/public/imagens/MasterCard.png" alt="MasterCard.png">
+    <img src="/public/imagens/VISA.png" alt="VISA.png">
+  </div>
+  <div>
+    &copy; Alguns direitos reservados. IFbooks 2025. 
+  </div>
+</footer>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
+  div.pagina1 div.mudar h1{
+    font-size: 2.5rem;
+  }
+  svg{
+    width: 20px;
+  }
+  p.apreco{
+    color: #27AE6099;
+  }
+  div.itens nav a{
+    text-decoration: none;
+    margin: 20px;
+    color: black;
+  }
+  div.icons i{
+    margin: 0 10px 10px 0 ;
+  }
+  div.icons span{
+    border-left: solid 1px;
+    margin: 0 10px 20px 0;
+  }
+  header span{
+    border-left: solid 1px;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+  }
+  section.lancamentos div.cima{
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    padding: 5vw;
+    justify-content: space-between;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
+  section.lancamentos div.baixo{
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    margin: 5vw;
+    justify-content: space-between;
   }
-}
+  section.lacamentos div.cima div h2{
+    font-size: 1.4rem;
+  }
+  section.mais div.itenss {
+    display: flex;
+    justify-content: space-between;
+    width: 70%;
+  }
+  section.mais hr{
+    border-bottom: solid 20px;
+  }
+  section.mais{
+    margin: 30px;
+  }
+  section
+  header div.icons{
+    color: #27AE60;
+  }
+  header div.itens nav a{
+    color: #7B7881;
+  }
+  header hr{
+    color: #27AE60;
+  }
+  footer{
+    background-color: #27AE60;
+    color: white;
+  }
+  footer div.ifbooks h2{
+    font-size: 0.9rem;
+  }
+
 </style>
